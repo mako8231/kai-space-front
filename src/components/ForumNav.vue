328dde644e901ab43e266167dbcf8e62410a2dcb
mako8231 <mm.neto2007@gmail.com>
2025-04-06 14:29:53 -0400

    cool looking additions

---
+++ src/components/ForumNav.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container-fluid">
+        <!--Sub Navbar-->
+        <div id="title-header" class="row">
+            <img class="logo" src="../assets/pictures/logo-brand.png" alt="">
+        </div>
+        <!--Main Navbar-->
+        <div id="nav-header" class="row">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid justify-content-end">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <!--Forum options -->
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <div class="navbar-nav container">
+                            <li class="nav-item">
+                                <a style="color: aliceblue;" class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a style="color: aliceblue;" class="nav-link" href="#">Forum</a>
+                            </li>
+                            <li class="nav-item">
+                                <a style="color: aliceblue;" class="nav-link" href="#">Latest Art</a>
+                            </li>
+                            <li class="nav-item">
+                                <a style="color: aliceblue;" class="nav-link" href="#">Imageboard</a>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid justify-content-lg-end justify-content-sm-center justify-content-xm-start">
+                     <form class="">
+                        <button class="button-login btn btn-outline-secondary me-2" type="button">Log-in</button>
+                        <button class="button-view-profile btn btn btn-outline-secondary me-2" type="button">View Profile</button>
+                        <!--If the authenticated user has mod/admin privileges-->
+                        <button class="button-admin-dashboard btn btn btn-outline-secondary me-2" type="button">Admin Dashboard</button>
+                        
+                    </form>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 
-
-import './assets/main.css'
-
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+
+import './assets/main.css'
 
 
 const app = createApp(App)
